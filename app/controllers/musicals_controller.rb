fe8070a9ae26bf8e5f3525daebbcf11aae04f74f
Hubert Lee <hubert.k.lee@gmail.com>
2014-12-07 00:02:37 +0800

    add devise and user authentication

--- app/controllers/musicals_controller.rb
+++ app/controllers/musicals_controller.rb
@@ -1,4 +1,6 @@
 class MusicalsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @musicals = Musical.where('title LIKE :query', query: "%#{params[:q]}%")
   end
